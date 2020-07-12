7cc9ba36d0be4f09e95cfe176e76683dc0fd1b6e
Francisco Castillo <fcastillog@ripley.com>
2020-07-12 11:05:19 -0400

    FIX - scene model api consumer

--- src/views/scene/model.js
+++ src/views/scene/model.js
@@ -3,21 +3,21 @@ import { getFromApi, postToApi } from '@services'
 import Settings from '@services/Settings'
 import { onSuccess, onError, onPending } from '@services/Common'
 
-export const onGetExample = () => {
-  return getFromApi(
-    Settings.buildUrl(Settings.urls.apiUrl, Settings.path.apiPath, {
+export const onGetExample = () =>
+  getFromApi({
+    url: Settings.buildUrl(Settings.urls.apiUrl, Settings.path.apiPath, {
       query: 'string',
     }),
     onSuccess,
     onError,
     onPending,
-  )
-}
+  })
+
 export const onPostExample = (body) =>
-  postToApi(
+  postToApi({
     body,
-    Settings.buildUrl(Settings.urls.apiUrl, Settings.path.composedPath('example')),
+    url: Settings.buildUrl(Settings.urls.apiUrl, Settings.path.composedPath('example')),
     onSuccess,
     onError,
     onPending,
-  )
+  })
